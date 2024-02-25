6bd2d77ac501f73ee99dc187044547f0d651a0a0
k0robova <alonakorobova@ukr.net>
2024-02-25 11:51:54 +0200

    +frontend

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,33 +1,31 @@
-import jwt from 'jsonwebtoken';
-import HttpError from '../helpers/HttpError.js';
-import { UserModel } from '../db/models/userModels.js';
+import dotenv from "dotenv";
+dotenv.config();
+import jwt from "jsonwebtoken";
+import HttpError from "../helpers/HttpError.js";
+import { UserModel } from "../db/models/userModels.js";
 
 const { SECRET_KEY } = process.env;
 
 export const authenticate = async (req, res, next) => {
-  const { authorization = '' } = req.headers;
+  const { authorization = "" } = req.headers;
 
-  const [bearer, token] = authorization.split(' ');
+  const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== 'Bearer') {
-    console.log('1');
-    next(HttpError(401, 'Not authorized'));
+  if (bearer !== "Bearer") {
+    next(HttpError(401, "Not authorized"));
   }
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await UserModel.findById(id);
-    console.log(user);
 
     if (!user || !user.token || user.token !== token) {
-      console.log('2');
-      next(HttpError(401, 'Not authorized'));
+      next(HttpError(401, "Not authorized"));
     }
 
     req.user = user;
     next();
   } catch (error) {
-    console.log(error);
-    next(HttpError(401, 'Not authorized'));
+    next(HttpError(401, "Not authorized"));
   }
 };
